a978e46955c4aeacbd751b4834f3bb2434608871
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2024-01-05 10:31:42 +0100

    added enablemongodb annotation in main

--- src/main/java/org/visionFive/Main.java
+++ src/main/java/org/visionFive/Main.java
@@ -3,11 +3,13 @@ package org.visionFive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.visionFive.controller.TodoController;
 import org.visionFive.data.repository.TodoListRepository;
 import org.visionFive.dto.request.UpdateRequest;
 import org.visionFive.services.TodoListServiceImpl;
 import org.visionFive.util.Date;
+@EnableMongoRepositories
 
 @SpringBootApplication
 public class Main {
